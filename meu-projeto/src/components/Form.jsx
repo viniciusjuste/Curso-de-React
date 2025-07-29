8ed156f9c945833305122ad7797a54077aac3895
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-07-29 19:11:20 -0300

    feat: usando o web Hook useState do React

--- meu-projeto/src/components/Form.jsx
+++ meu-projeto/src/components/Form.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const Form = () => {
 
     const cadastrarUser = (e) => {
@@ -6,13 +8,21 @@ const Form = () => {
         console.log(name);
     }
 
+    const [name, setName] = useState();
+    const [password, setPassword] = useState();
+
     return (
         <div>
             <h1>Meu Formulário</h1>
             <form onSubmit={cadastrarUser}>
                 <label>
                     Nome:
-                    <input type="text" name="name" />
+                    <input type="text" name="name" id="name" onChange={(e) => setName(e.target.value)} />
+                </label>
+
+                <label>
+                    Senha:
+                    <input type="password" name="password" id="password" onChange={(e) => setPassword(e.target.value)} />
                 </label>
                 <button type="submit">Enviar</button>
             </form>
