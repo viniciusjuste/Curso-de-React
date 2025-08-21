5fa1d6efadb84e8676683454c30bd95ba199c513
Vinícius Juste <157420992+viniciusjuste@users.noreply.github.com>
2025-08-21 18:24:55 -0300

    Renderização de listas

--- meu-projeto/src/App.js
+++ meu-projeto/src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Condicional from './components/Condicional';
+import OutraLista from './components/OutraLista';
 
 function App() {
-  const name = "Maria"
-
+const meusItens = ['React', 'Vue', 'Angular'];
   return (
     <div className="App">
-      <h1>Renderização condicional</h1>
-      <Condicional />
+      <h1>Renderização de listas</h1>
+      <OutraLista itens={[]} />
     </div>
   );
 }
---
+++ meu-projeto/src/components/OutraLista.jsx
@@ -0,0 +1,16 @@
+const OutraLista = ({ itens }) => {
+    return (
+        <>
+            <h3>Lista de coisas</h3>
+            {
+                itens.length > 0 ?
+                    (itens.map((item, index) => (
+                        <p key={index}>{item}</p>
+                    ))) :
+                    (<p>Lista vazia</p>)
+            }
+        </>
+    );
+};
+
+export default OutraLista;
